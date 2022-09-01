1712873e51d1e0ba3977fc165c44f8b37c7bdf1c
SDHK <630168819@qq.com>
2022-09-02 01:16:22 +0800

    重新思考系统实现思路

--- WorldTreeFramework/WorldTree/Core/System/SystemBase/System.cs
+++ WorldTreeFramework/WorldTree/Core/System/SystemBase/System.cs
@@ -27,9 +27,56 @@ namespace WorldTree
     /// <summary>
     /// 系统基类
     /// </summary>
-    public abstract class SystemBase<T,S> : ISystem
+    public abstract class SystemBase<T, S> : ISystem
     {
         public virtual Type EntityType => typeof(T);
         public virtual Type SystemType => typeof(S);
     }
+
+
+    public abstract class SendSystem<T> : SystemBase<T, SendSystem<T>>, ISystem
+    where T : class
+    {
+
+        public void Send(object self) => Event(self as T);
+        public abstract void Event(T self);
+    }
+
+
+    public abstract class SendSystem<T,T2> : SystemBase<T, SendSystem<T, T2>>, ISystem
+    where T : class
+    {
+        public void Send(object self,T2 t2) => Event(self as T,t2);
+        public abstract void Event(T self, T2 t2);
+    }
+
+
+    //接口实现的作用,调用的还是 统一转换成SendSystem基类
+    public abstract class SendEventSystem : SendSystem<EntityManager, float>
+    {
+        public override void Event(EntityManager self, float f)
+        {
+        }
+    }
+    //思路为 e.send<IEsystem>(1); 
+    //标签继承的是抽象类，然后以类名当接口 ，这样就算没实现也能调用，oop没实现是写不了这句的
+    //考虑直接用泛型接口？
+
+    // void send<sys,T,T2>(this T self,T2 t2){}
+    //fore root.systemManager.GetSystem<sys>()
+
+    // as SendSystem<T,T2> sys.Send(self,t2);
+
+
+    public static class Event2 {
+
+        public static void send<T,T2>(this T self ,T2 t2)
+            where T : class
+        {
+
+
+        }
+
+    }
+
 }
