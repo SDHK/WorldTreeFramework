d4df8a753314106402ea0f8163484e89e400c8c2
wujingsi <630168819@qq.com>
2022-10-17 10:43:40 +0800

    整理类型

---
+++ WorldTreeFramework/UnityEnvironment/EditorTool/EditorToolKitWindow/ToolPages/ScriptableObjectEditor/EditorAssetField.cs
@@ -0,0 +1,34 @@
+﻿using Sirenix.OdinInspector;
+using System;
+
+namespace WorldTree
+{
+    [Serializable]
+    public class EditorAssetField
+    {
+        [HideLabel, HorizontalGroup("类型")]
+        public EditorFieldAllType FieldType;
+
+        [ShowIf("FieldType", Value = EditorFieldAllType.Dictionary)]
+        [HideLabel, HorizontalGroup("类型")]
+        public EditorFieldType KeyType;
+
+        [ShowIf("FieldType", Value = EditorFieldAllType.Dictionary)]
+        [HideLabel, HorizontalGroup("类型")]
+        public EditorFieldType ValueType;
+
+        [ShowIf("FieldType", Value = EditorFieldAllType.List)]
+        [HideLabel, HorizontalGroup("类型")]
+        public EditorFieldType ItemType;
+
+        [HideLabel, HorizontalGroup("类型")]
+        [ShowIf("FieldType", Value = EditorFieldAllType.其它)]
+        public string TypeName;
+
+        [HideLabel, HorizontalGroup("名称")]
+        public string FieldName;
+        [HideLabel, HorizontalGroup("注释")]
+        public string Comment;
+
+    }
+}
---
+++ WorldTreeFramework/UnityEnvironment/EditorTool/EditorToolKitWindow/ToolPages/ScriptableObjectEditor/ListDictionary.cs
@@ -0,0 +1,49 @@
+﻿using Sirenix.OdinInspector;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WorldTree
+{
+
+    [Serializable]
+    public struct KeyValue<K, V>
+    {
+        [ShowInInspector]
+        public K key;
+        [ShowInInspector]
+        public V value;
+    }
+
+    [Serializable]
+    public class ListDictionary<Key, Value>
+    {
+        [TableList]
+        public List<KeyValue<Key, Value>> List;
+        [HideInInspector]
+        private Dictionary<Key, Value> dictionary;
+        [HideInInspector]
+        public Dictionary<Key, Value> Dictionary
+        {
+            get
+            {
+                if (List.Count != Dictionary.Count)
+                {
+                    ToDictionary();
+                }
+                return dictionary;
+            }
+        }
+
+        /// <summary>
+        /// List转为字典
+        /// </summary>
+        public void ToDictionary()
+        {
+            foreach (var item in List)
+            {
+                dictionary.TryAdd(item.key, item.value);
+            }
+        }
+    }
+}
