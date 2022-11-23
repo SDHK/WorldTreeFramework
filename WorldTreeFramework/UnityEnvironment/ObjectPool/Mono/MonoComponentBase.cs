7c27109bd20f95a5561ae9a26de6bc793f27aa60
wujingsi <630168819@qq.com>
2022-11-23 20:58:22 +0800

    思考实体组件与mono组件绑定

---
+++ WorldTreeFramework/UnityEnvironment/ObjectPool/Mono/MonoComponentBase.cs
@@ -0,0 +1,23 @@
+﻿
+/****************************************
+
+* 作者： 闪电黑客
+* 日期： 2022/11/23 20:39
+
+* 描述： Mono组件基类
+* 
+* 思考：entity绑定Mono 事件，是否由mono反向调用组件
+
+*/
+
+using UnityEngine;
+namespace WorldTree
+{
+    public abstract class MonoComponentBase : MonoBehaviour
+    {
+        /// <summary>
+        /// 绑定的实体
+        /// </summary>
+        public Entity entity;
+    }
+}
