959a97e93df5baaf692f94b44e70fabdd63c0336
wujingsi <630168819@qq.com>
2023-03-21 20:57:23 +0800

    准备开始写数值绑定

---
+++ WorldTreeFramework/WorldTree/Core/ObjectBox/Value/TreeFloat.cs
@@ -0,0 +1,28 @@
+﻿/****************************************
+
+* 作者： 闪电黑客
+* 日期： 2023/3/21 20:55
+
+* 描述： 树节点float类型
+
+*/
+
+namespace WorldTree
+{
+
+    /// <summary>
+    /// 树节点float类型
+    /// </summary>
+    public class TreeFloat : Node, ChildOfNode
+    {
+        /// <summary>
+        /// 值
+        /// </summary>
+        public float value;
+
+        /// <summary>
+        /// 法则执行器
+        /// </summary>
+        public RuleActuator ruleActuator;
+    }
+}
---
+++ WorldTreeFramework/WorldTree/Core/ObjectBox/Value/TreeInt.cs
@@ -0,0 +1,44 @@
+﻿/****************************************
+
+* 作者： 闪电黑客
+* 日期： 2023/3/21 20:42
+
+* 描述： 树节点Int类型
+
+*/
+
+namespace WorldTree
+{
+    /// <summary>
+    /// 树节点int类型
+    /// </summary>
+    public class TreeInt : Node, ChildOfNode
+    {
+        /// <summary>
+        /// 值
+        /// </summary>
+        public int value;
+
+        /// <summary>
+        /// 法则执行器
+        /// </summary>
+        public RuleActuator ruleActuator;
+    }
+
+
+    /// <summary>
+    /// 树节点float类型
+    /// </summary>
+    public class TreeFloat : Node, ChildOfNode
+    {
+        /// <summary>
+        /// 值
+        /// </summary>
+        public float value;
+
+        /// <summary>
+        /// 法则执行器
+        /// </summary>
+        public RuleActuator ruleActuator;
+    }
+}
