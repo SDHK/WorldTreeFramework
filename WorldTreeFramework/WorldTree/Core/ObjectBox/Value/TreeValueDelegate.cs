194a6783b780a77bbe65165877e565a2076516e2
wujingsi <630168819@qq.com>
2023-04-06 11:32:14 +0800

    泛型式树值改成绑定对象+泛型的方式，去除GC.

--- WorldTreeFramework/WorldTree/Core/ObjectBox/Value/TreeValueDelegate.cs
+++ WorldTreeFramework/WorldTree/Core/ObjectBox/Value/TreeValueDelegate.cs
@@ -15,23 +15,33 @@ namespace WorldTree
     /// 委托式泛型树值类型
     /// </summary>
     public class TreeValueDelegate<T> : TreeValueBase<T>
-        , IAwake<Func<T>, Action<T>>
+        , IAwake<object, Func<object, T>, Action<object, T>>
 
         , ChildOf<INode>
 
     where T : struct, IEquatable<T>
     {
-        public Func<T> Get;
-        public Action<T> Set;
+        /// <summary>
+        /// 绑定的对象
+        /// </summary>
+        public object m_BindObject;
+        /// <summary>
+        /// 从绑定对象上获取值
+        /// </summary>
+        public Func<object, T> m_Get;
+        /// <summary>
+        /// 将值设置到绑定对象上
+        /// </summary>
+        public Action<object, T> m_Set;
         public override T Value
         {
-            get => Get();
+            get => m_Get(m_BindObject);
 
             set
             {
-                if (Get().Equals(value))
+                if (m_Get(m_BindObject).Equals(value))
                 {
-                    this.Set(value);
+                    m_Set(m_BindObject, value);
                     m_RuleActuator?.Send(value);
                     m_GlobalRuleActuator?.Send(value);
                 }
