187d41c220a5c0e2a1e50827372541b4ea76c9ec
wujingsi <630168819@qq.com>
2023-05-25 20:25:39 +0800

    动态节点队列法则合并到节点文件

---
+++ WorldTreeFramework/WorldTree/Core/ObjectPool/NodePool/NodeListener/DynamicNodeListenerGroup.cs
@@ -0,0 +1,40 @@
+﻿
+/****************************************
+
+* 作者： 闪电黑客
+* 日期： 2023/5/25 18:00
+
+* 描述： 动态节点监听器集合
+
+*/
+
+using System;
+using Unity.VisualScripting;
+
+namespace WorldTree
+{
+    /// <summary>
+    /// 动态节点监听器集合
+    /// </summary>
+    public class DynamicNodeListenerGroup : Node, ComponentOf<NodePool>
+    {
+        public TreeDictionary<Type, RuleActuator> actuatorDictionary;
+
+
+    }
+
+    public static class DynamicNodeListenerGroupRule
+    {
+
+        public static void TrySendDynamicNodeListener(this INode node)
+        {
+            if (node.Core.NodePoolManager != null)
+                if (node.Core.NodePoolManager.TryGetPool(node.Type, out NodePool nodePool))
+                {
+                    nodePool.AddComponent(out DynamicNodeListenerGroup dynamicNodeListenerGroup);
+
+                }
+
+        }
+    }
+}
---
+++ WorldTreeFramework/WorldTree/Core/ObjectPool/NodePool/NodeListener/StaticNodeListenerGroup.cs
@@ -0,0 +1,68 @@
+﻿
+/****************************************
+
+* 作者： 闪电黑客
+* 日期： 2023/5/25 17:59
+
+* 描述： 静态节点监听器集合
+
+*/
+
+using System;
+
+namespace WorldTree
+{
+    /// <summary>
+    /// 静态节点监听器集合
+    /// </summary>
+    public class StaticNodeListenerGroup : Node, ComponentOf<NodePool>
+    {
+        public TreeDictionary<Type, RuleActuator> actuatorDictionary;
+
+    }
+
+
+    public static class StaticNodeListenerGroupRule
+    {
+        public static void TrySendStaticNodeListener(this INode node)
+        {
+            if (node.Core.NodePoolManager != null)
+                if (node.Core.NodePoolManager.TryGetPool(node.Type, out NodePool nodePool))
+                {
+                    nodePool.AddComponent(out StaticNodeListenerGroup staticNodeListenerGroup);
+                    //staticNodeListenerGroup
+
+                }
+
+        }
+
+
+        public static bool TryAddRuleActuator<R>(this StaticNodeListenerGroup self, Type Target, out IRuleActuator<R> actuator)
+            where R : IListenerRule
+        {
+            Type ruleType = typeof(R);
+
+
+            if (self.actuatorDictionary.TryGetValue(ruleType, out RuleActuator ruleActuator))
+            {
+                actuator = ruleActuator as IRuleActuator<R>; return true;
+            }
+            else if (self.Core.RuleManager.TryGetTargetRuleGroup(ruleType, Target, out var ruleGroup))
+            {
+
+            }
+
+
+
+            actuator = default;
+            return false;
+        }
+
+
+
+    }
+
+
+
+
+}
