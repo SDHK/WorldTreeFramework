4ccf88f340b924516a90767dcc33cf093571cd83
wujingsi <630168819@qq.com>
2023-10-21 15:38:41 +0800

    节点激活改为遍历分支

--- Unity/Assets/WorldTreeFramework/WorldTree/Core/Node/RuleMethod/NodeActiveRule.cs
+++ Unity/Assets/WorldTreeFramework/WorldTree/Core/Node/RuleMethod/NodeActiveRule.cs
@@ -12,58 +12,53 @@
 
 namespace WorldTree
 {
-    public static class NodeActiveRule
-    {
+	public static class NodeActiveRule
+	{
 
-        /// <summary>
-        /// 设置激活状态
-        /// </summary>
-        public static void SetActive(this INode self, bool value)
-        {
-            if (self.ActiveToggle != value)
-            {
-                self.ActiveToggle = value;
+		/// <summary>
+		/// 设置激活状态
+		/// </summary>
+		public static void SetActive(this INode self, bool value)
+		{
+			if (self.ActiveToggle != value)
+			{
+				self.ActiveToggle = value;
+				self.RefreshActive();
+			}
+		}
 
-                if (self.IsActive != ((self.Parent == null) ? self.ActiveToggle : self.Parent.IsActive && self.ActiveToggle))
-                {
-                    self.RefreshActive();
-                }
-            }
-        }
+		/// <summary>
+		/// 刷新激活状态：层序遍历设置子节点
+		/// </summary>
+		public static void RefreshActive(this INode self)
+		{
+			//如果状态相同，不需要刷新
+			if (self.IsActive == ((self.Parent == null) ? self.ActiveToggle : self.Parent.IsActive && self.ActiveToggle)) return;
 
-        /// <summary>
-        /// 刷新激活状态：层序遍历设置子节点
-        /// </summary>
-        public static void RefreshActive(this INode self)
-        {
-            using (self.PoolGet(out UnitQueue<INode> queue))
-            {
-                queue.Enqueue(self);
-                while (queue.Count != 0)
-                {
-                    var current = queue.Dequeue();
-                    if (current.IsActive != ((current.Parent == null) ? current.ActiveToggle : current.Parent.IsActive && current.ActiveToggle))
-                    {
-                        current.IsActive = !current.IsActive;
-                        if (current.m_Components != null)
-                        {
-                            foreach (var item in current.m_Components)
-                            {
-                                queue.Enqueue(item.Value);
-                            }
-                        }
+			//层序遍历设置子节点
+			using (self.PoolGet(out UnitQueue<INode> queue))
+			{
+				queue.Enqueue(self);
+				while (queue.Count != 0)
+				{
+					var current = queue.Dequeue();
+					if (current.IsActive != ((current.Parent == null) ? current.ActiveToggle : current.Parent.IsActive && current.ActiveToggle))
+					{
+						current.IsActive = !current.IsActive;
 
-                        if (current.m_Children != null)
-                        {
-                            foreach (var item in current.m_Children)
-                            {
-                                queue.Enqueue(item.Value);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-    }
+						if (current.m_Branchs != null)
+						{
+							foreach (var branchs in current.m_Branchs)
+							{
+								foreach (INode node in branchs.Value)
+								{
+									queue.Enqueue(node);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
