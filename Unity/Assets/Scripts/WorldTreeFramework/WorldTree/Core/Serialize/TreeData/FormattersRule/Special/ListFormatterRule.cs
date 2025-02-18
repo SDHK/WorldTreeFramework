5fb49d95e6f2df98fd520de050b78927cd84cc4b
SDHK <630168819@qq.com>
2025-02-18 15:02:43 +0800

    List序列化遗漏标记补充，代码整理

--- Unity/Assets/Scripts/WorldTreeFramework/WorldTree/Core/Serialize/TreeData/FormattersRule/Special/ListFormatterRule.cs
+++ Unity/Assets/Scripts/WorldTreeFramework/WorldTree/Core/Serialize/TreeData/FormattersRule/Special/ListFormatterRule.cs
@@ -7,13 +7,13 @@
 
 */
 
-using System;
 using System.Collections.Generic;
 
 namespace WorldTree.TreeDataFormatters
 {
 	public static class ListFormatterRule
 	{
+		[TreeDataSpecial(1)]
 		private class Serialize<T> : ICollectionSpecialFormatterRule.SerializeBase<List<T>, T> { }
 
 		private class Deserialize<T> : ICollectionSpecialFormatterRule.DeserializeBase<List<T>, T> { }
