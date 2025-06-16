0718ed84ee17ce1bd43c665b0ad58ca6b9d7c2a5
SDHK <630168819@qq.com>
2025-06-16 19:51:08 +0800

    winform分析器配置

---
+++ WinForm/Analysis/ProjectAnalyzerSetting.cs
@@ -0,0 +1,85 @@
+/****************************************
+
+* 作者：闪电黑客
+* 日期：2024/6/18 16:42
+
+* 描述：
+
+*/
+
+using System.Collections.Generic;
+
+namespace WorldTree.Analyzer
+{
+	/// <summary>
+	/// 分析器设置
+	/// </summary>
+	public static class ProjectConfigHelper
+	{
+		public static List<DiagnosticConfigGroup> CoreConfigs = new()
+		{
+			new ListDiagnosticConfig(),
+			new ArrayDiagnosticConfig(),
+			new DictionaryDiagnosticConfig(),
+			new HashSetDiagnosticConfig(),
+			new QueueDiagnosticConfig(),
+			new StackDiagnosticConfig(),
+
+			new CodeNodeDiagnosticConfig(),
+			new RuleDiagnosticConfig(),
+			new StaticRuleDiagnosticConfig(),
+			new ObjectDiagnosticConfig()
+		};
+
+		public static List<DiagnosticConfigGroup> NodeConfigs = new()
+		{
+			new ListDiagnosticConfig(),
+			new ArrayDiagnosticConfig(),
+			new DictionaryDiagnosticConfig(),
+			new HashSetDiagnosticConfig(),
+			new QueueDiagnosticConfig(),
+			new StackDiagnosticConfig(),
+
+
+			new NodeDiagnosticConfig(),
+			new CodeNodeDiagnosticConfig(),
+			new RuleDiagnosticConfig(),
+			new StaticRuleDiagnosticConfig(),
+			//new ProjectBanUnitDiagnosticConfig(), //由于Rule也继承了Unit，所以这里不再需要
+			new ObjectDiagnosticConfig()
+		};
+
+		public static List<DiagnosticConfigGroup> RuleConfigs = new()
+		{
+			new ListDiagnosticConfig(),
+			new ArrayDiagnosticConfig(),
+			new DictionaryDiagnosticConfig(),
+			new HashSetDiagnosticConfig(),
+			new QueueDiagnosticConfig(),
+			new StackDiagnosticConfig(),
+
+			new RuleSwitchAttributeDiagnosticConfig(),
+			new NodeRuleAttributeDiagnosticConfig(),
+
+			new CodeNodeDiagnosticConfig(),
+			new RuleDiagnosticConfig(),
+			new StaticRuleDiagnosticConfig(),
+			//new ProjectBanDiagnosticConfig(),
+			new ObjectDiagnosticConfig()
+		};
+	}
+
+	/// <summary>
+	/// 控制台环境分析器配置
+	/// </summary>
+	public class ProjectAnalyzerSetting : ProjectDiagnosticConfig
+	{
+		public ProjectAnalyzerSetting()
+		{
+			Add("WinForm.Core", ProjectConfigHelper.CoreConfigs);
+			Add("Node", ProjectConfigHelper.NodeConfigs);
+			Add("Rule", ProjectConfigHelper.RuleConfigs);
+			Add("Config", ProjectConfigHelper.CoreConfigs);
+		}
+	}
+}
---
+++ WinForm/Analysis/ProjectGeneratorSetting.cs
@@ -0,0 +1,60 @@
+﻿/****************************************
+
+* 作者：闪电黑客
+* 日期：2025/6/13 14:42
+
+* 描述：
+
+*/
+using System;
+using System.Collections.Generic;
+
+namespace WorldTree.SourceGenerator
+{
+	/// <summary>
+	/// 生成器设置
+	/// </summary>
+	public static class ProjectConfigHelper
+	{
+		public static HashSet<Type> CoreConfigs = new() {
+			typeof(BranchSupplementGeneratorRun) ,
+			typeof(CopyNodeClassGeneratorRun),
+			typeof(NodeExtensionMethodGeneratorRun),
+			typeof(NodeBranchHelperGeneratorRun),
+			typeof(RuleClassGeneratorRun),
+
+			typeof(RuleSupplementGeneratorRun),
+			typeof(RuleMethodGeneratorRun),
+			typeof(RuleExtensionMethodGeneratorRun),
+			typeof(TreeCopyGeneratorRun),
+			typeof(TreeDataSerializeGeneratorRun),
+			typeof(TreePackSerializeGeneratorRun),
+		};
+		public static HashSet<Type> NodeConfigs = new()
+		{
+			typeof(RuleSupplementGeneratorRun),
+			typeof(TreeCopyGeneratorRun),
+			typeof(TreeDataSerializeGeneratorRun),
+			typeof(TreePackSerializeGeneratorRun),
+		};
+		public static HashSet<Type> RuleConfigs = new()
+		{
+			typeof(RuleMethodGeneratorRun),
+		};
+	}
+
+	/// <summary>
+	/// 控制台环境生成器配置
+	/// </summary>
+	public class ProjectGeneratorSetting : ProjectGeneratorsConfig
+	{
+		public ProjectGeneratorSetting()
+		{
+			ArgumentCount = 5;
+			Add("WinForm.Core", ProjectConfigHelper.CoreConfigs);
+			Add("Node", ProjectConfigHelper.NodeConfigs);
+			Add("Rule", ProjectConfigHelper.RuleConfigs);
+			Add("Config", ProjectConfigHelper.CoreConfigs);
+		}
+	}
+}
