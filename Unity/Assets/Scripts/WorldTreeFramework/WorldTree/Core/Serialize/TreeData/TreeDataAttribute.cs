0c67d4b1f4132b6cb7c6abf8b39bd22945754f6c
SDHK <630168819@qq.com>
2026-05-09 18:40:13 +0800

    注释补充

--- Unity/Assets/Scripts/WorldTreeFramework/WorldTree/Core/Serialize/TreeData/TreeDataAttribute.cs
+++ Unity/Assets/Scripts/WorldTreeFramework/WorldTree/Core/Serialize/TreeData/TreeDataAttribute.cs
@@ -27,14 +27,22 @@ namespace WorldTree
 	}
 
 	/// <summary>
-	/// 序列化成员忽略标记
+	/// 序列化成员忽略标记 
 	/// </summary>
 	[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
 	public class TreeDataIgnoreAttribute : Attribute { }
 
 	/// <summary>
-	/// 树节点特别处理标记，参数为设定的字段数量
+	/// 树节点特别处理标记，参数为设定的字段数量。标记「集合 / 特殊容器」
 	/// </summary>
+	/// <remarks>
+	/// 声明集合泛型在源生成器中对应的「字段数」
+	/// 避免继承该集合的子类把基类实现细节当成多个持久化字段。
+	/// 
+	/// 通过子类扫描到父级有该特性后，源生成器会把特性标记数量当成父级字段数量，
+	/// 而不是根据成员类型的字段数量来处理。
+	/// </remarks>
+	[AttributeUsage(AttributeTargets.Class)]
 	public class TreeDataSpecialAttribute : Attribute
 	{
 		/// <summary>
@@ -42,7 +50,7 @@ namespace WorldTree
 		/// </summary>
 		public int FieldsCount;
 
-		public TreeDataSpecialAttribute(int fieldsCount = 1)
+		public TreeDataSpecialAttribute(int fieldsCount)
 		{
 			FieldsCount = fieldsCount;
 		}
